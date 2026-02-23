a9f5283c57aa29cc59a59384f893df2c761fbe7b
umwwwelt (Yann Bougaret) <y.bougaret@pm.me>
2026-02-23 10:40:41 +0100

    feat: modify m, add chezmoi ignore

--- dot_config/fish/functions/m.fish
+++ dot_config/fish/functions/m.fish
@@ -16,6 +16,22 @@ function m --description "cd to ~/code/murmure"
         return $status
     end
 
+    set -l best ""
+    set -l best_ts 0
+    for dir in "$base/$argv[1]"*/
+        test -d "$dir" || continue
+        set -l ts (git -C "$dir" log -1 --format="%ct" 2>/dev/null)
+        test -z "$ts" && set ts 0
+        if test "$ts" -gt "$best_ts"
+            set best_ts "$ts"
+            set best "$dir"
+        end
+    end
+    if test -n "$best"
+        cd "$best"
+        return $status
+    end
+
     echo "m: not found: $target"
     return 1
 end
