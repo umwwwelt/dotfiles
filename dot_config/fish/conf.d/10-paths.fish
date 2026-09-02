2a3d479bf0dd1bb436e64b704e6e0c6c4fae9ff7
umwwwelt (Yann Bougaret) <y.bougaret@pm.me>
2026-09-02 15:25:49 +0200

    fix(fish): load fnm after path block, drop pinned nvm path
    05-fnm.fish etait charge avant 10-paths.fish, qui prependait
    $HOME/.nvm/versions/node/v22.14.0/bin au PATH et masquait donc fnm
    a chaque nouveau shell (node v22.14.0 au lieu du default fnm v24.13.1).
    
    Renomme en 15-fnm.fish pour que 'fnm env' prenne la main en dernier,
    et supprime le chemin nvm epingle : nvm est desinstalle.

--- dot_config/fish/conf.d/10-paths.fish
+++ dot_config/fish/conf.d/10-paths.fish
@@ -9,9 +9,6 @@ fish_add_path -g $HOME/.cargo/bin
 # npm global (si utilisé)
 fish_add_path -g $HOME/.npm-global/bin
 
-# nvm : version épinglée, ignorée si elle n'est plus installée
-set -l nvm_node $HOME/.nvm/versions/node/v22.14.0/bin
-test -d $nvm_node && fish_add_path -g $nvm_node
 
 # pnpm (les binaires globaux sont directement dans PNPM_HOME)
 fish_add_path -g $HOME/Library/pnpm
