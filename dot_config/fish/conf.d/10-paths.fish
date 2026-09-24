3c56e432b6f35495d3f3da34a0b301da65c61b7f
umwwwelt (Yann Bougaret) <y.bougaret@pm.me>
2026-09-24 13:14:18 +0200

    fix: path

--- dot_config/fish/conf.d/10-paths.fish
+++ dot_config/fish/conf.d/10-paths.fish
@@ -10,8 +10,8 @@ fish_add_path -g $HOME/.cargo/bin
 fish_add_path -g $HOME/.npm-global/bin
 
 
-# pnpm (les binaires globaux sont directement dans PNPM_HOME)
-fish_add_path -g $HOME/Library/pnpm
+# pnpm >= 11 : binaires globaux dans PNPM_HOME/bin
+fish_add_path -g $HOME/Library/pnpm/bin
 
 #bun
 fish_add_path -g "$HOME/.bun/bin"
